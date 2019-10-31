ed0bc70fbed1d30c732811744f9decc403815dfd
Ramesh Chellaton <rameshchellaton@rameshs-mbp.lan>
2019-10-30 23:24:34 -0400

    Comparing LA and Baltimore(MD) data.

--- plot6.R
+++ plot6.R
@@ -4,7 +4,7 @@ library(ggplot2)
 # get list of Motor Vehicles related codes from SCC.rds
 #SCC <- readRDS("Source_Classification_Code.rds")
 #NEI <- readRDS("summarySCC_PM25.rds")
-# filter for Baltimore
+# filter for Baltimore & LA
 baltimoreData <- subset(NEI, fips=="24510" | fips=="06037")
 motorSCC <- (SCC[grep("Motor",SCC$Short.Name),1:3])
 # join Baltimore NEI data with motorSCC by SCC
@@ -15,11 +15,26 @@ motorNEIsumm <- summarize(motorNEIgroup, "Total_Emissions"=sum(Emissions))
 #
 # plot the graph in png file
 #
+baseLA <- motorNEIsumm %>% filter(year==1999 & fips=="06037")
+baseMD <- motorNEIsumm %>% filter(year==1999 & fips=="24510")
+
+LA <- motorNEIsumm %>% filter(fips=="06037")  %>% mutate(Total_Emissions/baseLA$Total_Emissions)
+MD <- motorNEIsumm %>% filter(fips=="24510")  %>% mutate(Total_Emissions/baseMD$Total_Emissions)
+names(LA) <- c(names(motorNEIsumm),"PCT")
+names(MD) <- c(names(motorNEIsumm),"PCT")
+motorNEIsumm <- rbind(LA, MD)
+
 png("plot6.png", height = 480, width = 480, type="quartz")
 p <- ggplot(motorNEIsumm, aes(year, Total_Emissions, group=fips, color=fips)) + 
-  ylab("Baltimore Emissions from Motor Vehicles in tons") +
+  ylab("Emissions from Motor Vehicles in tons") +
   geom_line() + geom_point()
+print(p)
+dev.off()
 
+png("plot6a.png", height = 480, width = 480, type="quartz")
+p <- ggplot(motorNEIsumm, aes(year, PCT, group=fips, color=fips)) + 
+  ylab("PCT Emissions from Motor Vehicles in tons") +
+  geom_line() + geom_point()
 print(p)
 dev.off()
 # close file
